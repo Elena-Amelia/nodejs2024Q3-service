996f1689f1f1b0c987b00690cf96eadcd8235e2d
Elena-Amelia <amelia013@ya.ru>
2024-11-09 19:32:40 +0400

    feat: implement user routing

---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Req, Post, Put, Body, Param, ParseUUIDPipe, Delete } from '@nestjs/common';
+import { Request } from 'express';
+import { UserService } from './user.service';
+import { User} from '../interfaces/interfaces';
+import { CreateUserDto, UpdatePasswordDto } from './dto/user.dto'
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getAllUsers(): User[] {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', new ParseUUIDPipe()) id:string): User {
+    return this.userService.getUserById(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto){
+    return this.userService.createUser(createUserDto)
+  }
+  @Put(':id')
+  updateUserPassword(@Body() updatePasswordDto: UpdatePasswordDto, @Param('id', new ParseUUIDPipe()) id:string): User {
+    return this.userService.updateUserPassword(updatePasswordDto, id);
+  }
+  @Delete(':id')
+  deleteUser(@Param('id', new ParseUUIDPipe()) id:string) {
+    return this.userService.deleteUser(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,82 @@
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
+import { User, users } from '../interfaces/interfaces';
+import { CreateUserDto, UpdatePasswordDto } from './dto/user.dto';
+import { v4, validate } from 'uuid';
+
+@Injectable()
+export class UserService {
+  createUser(dto: CreateUserDto) {
+    const user: User = {
+      id: v4(),
+      login: dto.login,
+      password: dto.password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    users.push(user);
+
+    const validUser = Object.assign({}, user);
+    delete validUser.password;
+    return validUser;
+  }
+
+  getAllUsers(): User[] {
+    const validUsers = users.map((user: User) => {
+      const validUser = Object.assign({}, user);
+      delete validUser.password;
+      return validUser;
+    });
+    return validUsers;
+  }
+
+  getUserById(id: string): User {
+    const existedUser = users.find((user) => user.id === id);
+
+    if (existedUser) {
+      const validUser = Object.assign({}, existedUser);
+      delete validUser.password;
+      return validUser;
+    } else {
+      throw new HttpException("User doesn't exist", HttpStatus.NOT_FOUND);
+    }
+  }
+
+  updateUserPassword(dto: UpdatePasswordDto, id: string): User {
+    const existedUser = users.find((user) => {
+      if (user.id === id) {
+        if (user.password === dto.oldPassword) {
+          user.password = dto.newPassword;
+          user.version++;
+          user.updatedAt = Date.now();
+          return true;
+        } else {
+          throw new HttpException(
+            'Old password is wrong',
+            HttpStatus.FORBIDDEN,
+          );
+        }
+      }
+    });
+
+    if (existedUser) {
+      const validUser = Object.assign({}, existedUser);
+      delete validUser.password;
+      return validUser;
+    } else {
+      throw new HttpException("User doesn't exist", HttpStatus.NOT_FOUND);
+    }
+  }
+  deleteUser(id: string) {
+    const existedUser = users.find((user, ind) => {
+      if (user.id === id) {
+        users.splice(ind, 1);
+        return true;
+      }
+    });
+
+    if (!existedUser) {
+      throw new HttpException("User doesn't exist", HttpStatus.NOT_FOUND);
+    }
+  }
+}
