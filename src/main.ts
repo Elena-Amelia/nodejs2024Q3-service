302f546eb947ba5f35b324e829fb85f19628ef8c
Elena-Amelia <amelia013@ya.ru>
2024-11-10 23:46:36 +0400

    feat: change & connect swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory} from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as yaml from 'yamljs';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const doc: OpenAPIObject = yaml.load('./doc/api.yaml');
+  SwaggerModule.setup('doc', app, doc);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+  
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
