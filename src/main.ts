b1c64795d293944c16e910baff92f5ba497d689e
Elena-Amelia <amelia013@ya.ru>
2024-11-11 23:36:15 +0400

    fix: fix bug with port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as yaml from 'yamljs';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
